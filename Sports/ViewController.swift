3e2067785627665f2ac6f7e2c356289a665089e9
Macos <macos@Macoss-MacBook-Pro.local>
2025-05-10 19:08:21 +0300

    first

---
+++ Sports/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sports
+//
+//  Created by Macos on 10/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
